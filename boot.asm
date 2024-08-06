fbca7bbd161b652ff5418be3e32c0ceac75b91b3
landonburns19 <112574454+landonburns19@users.noreply.github.com>
2024-08-06 13:46:04 +0000

    modified:   boot.asm 	modified:   boot.bin 	modified:   boot.iso

--- boot.asm
+++ boot.asm
@@ -1,3 +1,20 @@
+[org 0x7c00]
+mov ah, 0x0e
+mov bx, starting_message
+
+print_this:
+    mov al, [bx]
+    cmp al, 0
+    je end
+    int 0x10
+    inc bx
+    jmp print_this
+end:
+
 jmp $
+
+starting_message:
+    db "beginning", 0
+
 times 510-($-$$) db 0
-db 0x55, 0xaa
+dw 0xaa55
