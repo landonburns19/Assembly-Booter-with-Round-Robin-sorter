5a434a5e15cc33844f500e255927f52b9bc262e1
landonburns19 <112574454+landonburns19@users.noreply.github.com>
2024-09-22 06:22:05 +0000

    modified:   boot.asm 	modified:   boot.bin

--- boot.asm
+++ boot.asm
@@ -12,6 +12,13 @@ mov byte [31747+10], 1
 mov byte [31745+20], 'p'
 mov byte [31746+20], '3'
 mov byte [31747+20], 1
+
+mov byte [31747+30], 0
+mov byte [31747+31], 0
+mov byte [31747+32], 0
+
+
+
 jmp RRtimer
 end1:
 
@@ -49,6 +56,30 @@ cmp al, bl
 JL p3getsallocated
 jmp end1
 
+
+p1getsallocated:
+mov al, [31747+31]
+inc al
+mov byte [31747+31], al
+mov al, [31747+32]
+inc al
+mov byte [31747+32], al
+
+p2getsallocated:
+mov al, [31747+30]
+inc al
+mov byte [31747+30], al
+mov al, [31747+32]
+inc al
+mov byte [31747+32], al
+
+p3getsallocated:
+mov al, [31747+30]
+inc al
+mov byte [31747+30], al
+mov al, [31747+31]
+inc al
+mov byte [31747+31], al
 
 times 510-($-$$) db 0
 dw 0xaa55
